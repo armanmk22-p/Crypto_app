6a7ca28de378a7ac9df8d3dbeb031b1d514545fc
armanmk22-p <armanmk.programming22@gmail.com>
2023-03-29 21:56:13 -0700

    final changes

--- lib/my_home.dart
+++ lib/my_home.dart
@@ -1,12 +1,15 @@
 import 'package:crypto_app/controller.dart';
-
 import 'package:flutter/material.dart';
-
 import 'di/di.dart';
 
-class MyHome extends StatelessWidget {
+class MyHome extends StatefulWidget {
   MyHome({Key? key}) : super(key: key);
 
+  @override
+  State<MyHome> createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
   final crypro = getIt<HomeController>();
 
   @override
@@ -15,7 +18,7 @@ class MyHome extends StatelessWidget {
         child: Scaffold(
       appBar: AppBar(
         title: Text('Crypto Market'),
-        centerTitle: true,
+        automaticallyImplyLeading: false,
       ),
       body: FutureBuilder(
           future: crypro.getCryptos(),
@@ -31,7 +34,7 @@ class MyHome extends StatelessWidget {
               );
             } else if (snapshot.hasData) {
               return ListView.builder(
-                  itemCount: snapshot.data!.length,
+                  itemCount:snapshot.data!.length,
                   itemBuilder: (context, index) {
                     final newCrypto = snapshot.data![index];
                     return ListTile(
@@ -40,13 +43,21 @@ class MyHome extends StatelessWidget {
                           child: Center(
                             child: Text(newCrypto.rank.toString()),
                           )),
-                      title: Text(newCrypto.name.toString()),
+                      title: _CustomText(newCrypto.changePercent24Hr,newCrypto.name.toString()),
                       subtitle: Text(newCrypto.symbol.toString()),
+                      trailing: SizedBox(width: 45, child: Center(child: _CustomIcon(newCrypto.changePercent24Hr)),),
                     );
                   });
             }
             return Container();
           }),
     ));
+  }
+
+  Widget _CustomIcon(double change){
+    return change<=0 ? Icon(Icons.trending_down,color: Colors.red) : Icon(Icons.trending_up,color: Colors.green,);
+  }
+  Widget _CustomText(double change,String title){
+    return change<=0 ? Text(title,style: TextStyle(color: Colors.red),): Text(title,style: TextStyle(color: Colors.green),);
   }
 }
